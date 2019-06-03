827f49dfdd4073f9346c2b37c0efc894836380f7
zhangmingzhe <1245539137@qq.com>
2019-06-03 18:19:41 +0800

    write code for set range of bit with specified value

--- HandleBit/HandleBit/HandleBit.vb
+++ HandleBit/HandleBit/HandleBit.vb
@@ -1,5 +1,6 @@
 ﻿Public Class HandleBit
 
+    ' Set a bit to 1. input value length is 8.
     Public Shared Function SetOne(ByRef val As Byte, ByVal pos As Integer)
         Dim realPos As Integer
         realPos = pos
@@ -207,11 +208,75 @@
 
 
 
-    Public Shared Function SetBitsInRange(ByRef val As Byte, ByVal startBit As Integer, ByVal endBit As Integer)
+    Public Shared Function SetBitsInRange8(ByRef d As Byte, ByVal val As Byte, ByVal pos As Integer, ByVal len As Integer)
+        Try
+            Dim init1 = 0, init2 = 0
+            Dim Boundry = 8
+            pos = pos Mod 8
+            If len = 0 Or len > Boundry Then
+                Return False
+            End If
 
+
+            init1 = Convert.ToByte(2 ^ len - 1)
+            init2 = Not (init1 << pos)
+
+            val = val And init1
+            val <<= pos
+
+            d = d And init2
+            d = d Or val
+            Return True
+        Catch ex As Exception
+            Throw ex
+        End Try
     End Function
-    Public Shared Function SetBitsInRange(ByRef val As Byte, ByVal mask As String)
 
+    Public Shared Function SetBitsInRange16(ByRef d As UInt16, ByVal val As UInt16, ByVal pos As Integer, ByVal len As Integer)
+        Try
+            Dim init1 = 0, init2 = 0
+
+            Dim Boundry = 16
+            If len = 0 Or len > Boundry Then
+                Return False
+            End If
+
+            pos = pos Mod 16
+
+            init1 = Convert.ToUInt16(2 ^ len - 1)
+            init2 = Not (init1 << pos)
+
+            val = val And init1
+            val <<= pos
+
+            d = d And init2
+            d = d Or val
+            Return True
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+    Public Shared Function SetBitsInRange32(ByRef d As UInt32, ByVal val As Integer, ByVal pos As Integer, ByVal len As Integer)
+        Try
+            Dim init1 = 0, init2 = 0
+            Dim Boundry = 32
+
+            If len = 0 Or len > Boundry Then
+                Return False
+            End If
+
+            pos = pos Mod 16
+
+            init1 = Convert.ToUInt32(2 ^ len - 1)
+            init2 = Not (init1 << pos)
+            val = val And init1
+            val <<= pos
+            d = d And init2
+            d = d Or val
+            Return True
+        Catch ex As Exception
+            Throw ex
+        End Try
     End Function
 
 End Class
--- HandleBit/HandleBit/Module1.vb
+++ HandleBit/HandleBit/Module1.vb
@@ -2,8 +2,10 @@
 
     Sub Main()
         Dim val As UInt64 = &HFFFFE
+        Dim val16 As UInteger = Convert.ToUInt16(&HFFFFFFFF)
         Dim s = "1110"
-        Console.WriteLine(HandleBit.BinToDec(s))
+        HandleBit.SetBitsInRange32(val16, 0, 10, 3)
+
         While True
 
         End While
