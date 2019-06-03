bd243ecd5482177fb8dc89b563b659590a9bd3e5
zhangmingzhe <1245539137@qq.com>
2019-06-03 16:08:27 +0800

    Frame already init need more work here

---
+++ HandleBit/HandleBit/HandleBit.vb
@@ -0,0 +1,217 @@
+﻿Public Class HandleBit
+
+    Public Shared Function SetOne(ByRef val As Byte, ByVal pos As Integer)
+        Dim realPos As Integer
+        realPos = pos
+        If pos > (8 - 1) Then
+            realPos = (pos Mod 8)
+        End If
+        val = val Or (1 << realPos)
+    End Function
+
+    Public Shared Function SetOne(ByRef val As UInt16, ByVal pos As Integer)
+        Dim realPos As Integer
+        realPos = pos
+        If pos > (16 - 1) Then
+            realPos = (pos Mod 16)
+        End If
+        val = val Or (1 << realPos)
+    End Function
+
+    Public Shared Function SetOne(ByRef val As UInt32, ByVal pos As Integer)
+        Dim realPos As Integer
+        realPos = pos
+        If pos > (32 - 1) Then
+            realPos = (pos Mod 32)
+        End If
+        val = val Or (1 << realPos)
+    End Function
+    Public Shared Function SetOne(ByRef val As UInt64, ByVal pos As Integer)
+        Dim realPos As Integer
+        realPos = pos
+        If pos > (64 - 1) Then
+            realPos = (pos Mod 64)
+        End If
+        val = val Or (1 << realPos)
+    End Function
+
+    Public Shared Function SetZero(ByRef val As Byte, ByVal pos As Integer)
+        Dim realPos As Integer = pos
+        Dim mask = 1
+        If pos > (8 - 1) Then
+            realPos = pos Mod 8
+        End If
+        mask = mask << realPos
+        mask = Not mask
+        val = val And mask
+    End Function
+
+    Public Shared Function SetZero(ByRef val As UInt16, ByVal pos As Integer)
+        Dim realPos As Integer = pos
+        Dim mask = 1
+        If pos > (16 - 1) Then
+            realPos = pos Mod 16
+        End If
+        mask = mask << realPos
+        mask = Not mask
+        val = val And mask
+
+    End Function
+
+    Public Shared Function SetZero(ByRef val As UInt32, ByVal pos As Integer)
+        Dim realPos As Integer = pos
+        Dim mask = 1
+        If pos > (32 - 1) Then
+            realPos = pos Mod 32
+        End If
+        mask = mask << realPos
+        mask = Not mask
+        val = val And mask
+
+    End Function
+
+    Public Shared Function SetZero(ByRef val As UInt64, ByVal pos As Integer)
+        Dim realPos As Integer = pos
+        Dim mask = 1
+        If pos > (64 - 1) Then
+            realPos = pos Mod 64
+        End If
+        mask = mask << realPos
+        mask = Not mask
+        val = val And mask
+    End Function
+
+    Public Shared Function DecToBin(ByVal s As Byte) As String
+        Try
+            Dim res As String
+            Do
+                res = (Str(s Mod 2) & res)
+                s = s \ 2 ' divide exactly ,整除 , 割り切れる
+            Loop Until s = 0
+            Return res
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+    Public Shared Function DecToBin(ByVal s As UInt16) As String
+        Try
+            Dim res As String
+            Do
+                res = (Str(s Mod 2) & res)
+                s = s \ 2 ' divide exactly ,整除 , 割り切れる
+            Loop Until s = 0
+            Return res
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+    Public Shared Function DecToBin(ByVal s As UInt32) As String
+        Try
+            Dim res As String
+            Do
+                res = (Str(s Mod 2) & res)
+                s = s \ 2 ' divide exactly ,整除 , 割り切れる
+            Loop Until s = 0
+            Return res
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+    Public Shared Function DecToBin(ByVal s As UInt64) As String
+        Try
+            Dim res As String
+            Do
+                res = (Str(s Mod 2) & res)
+                s = s \ 2 ' divide exactly ,整除 , 割り切れる
+            Loop Until s = 0
+            Return res
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+    Public Shared Function HexToBin(ByVal s As Byte) As String
+        Try
+            Dim res As String
+            Do
+                res = (Str(s Mod 2) & res)
+                s = s \ 2 ' divide exactly ,整除 , 割り切れる
+            Loop Until s = 0
+            Return res
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+    Public Shared Function HexToBin(ByVal s As UInt16) As String
+        Try
+            Dim res As String
+            Do
+                res = (Str(s Mod 2) & res)
+                s = s \ 2 ' divide exactly ,整除 , 割り切れる
+            Loop Until s = 0
+            Return res
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+    Public Shared Function HexToBin(ByVal s As UInt32) As String
+        Try
+            Dim res As String
+            Do
+                res = (Str(s Mod 2) & res)
+                s = s \ 2 ' divide exactly ,整除 , 割り切れる
+            Loop Until s = 0
+            Return res
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+    Public Shared Function HexToBin(ByVal s As UInt64) As String
+        Try
+            Dim res As String
+            Do
+                res = (Str(s Mod 2) & res)
+                s = s \ 2 ' divide exactly ,整除 , 割り切れる
+            Loop Until s = 0
+            Return res
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+    Public Shared Function BinToDec(ByVal b As String) As Integer
+        Try
+            Dim sorted = b.ToList()
+
+            sorted.Sort()
+
+            If sorted.First <> "0" And sorted.First <> "1" Then
+                Throw New Exception("the binary string need only contain 0 or ")
+            End If
+            If sorted.Last <> "0" And sorted.Last <> "1" Then
+                Throw New Exception("the binary string need only contain 0 or 1")
+            End If
+
+
+            Dim index = 0
+            Dim res = 0
+            For Each item In b.Reverse()
+                res += Val(item) * (2 ^ index)
+                index += 1
+
+            Next
+            Return res
+
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+
+
+
+    Public Shared Function SetBitsInRange(ByRef val As Byte, ByVal startBit As Integer, ByVal endBit As Integer)
+
+    End Function
+    Public Shared Function SetBitsInRange(ByRef val As Byte, ByVal mask As String)
+
+    End Function
+
+End Class
---
+++ HandleBit/HandleBit/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim val As UInt64 = &HFFFFE
+        Dim s = "1110"
+        Console.WriteLine(HandleBit.BinToDec(s))
+        While True
+
+        End While
+    End Sub
+
+End Module
